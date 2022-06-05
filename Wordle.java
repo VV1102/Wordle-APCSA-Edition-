6994ba9eaf312d6faf4022cc71de96b853f108ef
VV1102 <105758475+VV1102@users.noreply.github.com>
2022-06-04 21:52:21 -0400

    Update and rename sudoku.java to Wordle.java

---
+++ Wordle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Wordle
+{   
+    public static void main(String[] args)
+    {
+       Game wordle1 = new Game("house" , 0);
+       (wordle1.get(wordOfTheDay)).wordle();
+    }
+}

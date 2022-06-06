b94895c432f3e50b65190ac8f939190371c8b4b8
VV1102 <105758475+VV1102@users.noreply.github.com>
2022-06-06 08:03:38 -0400

    Create OtherStuff.java

---
+++ OtherStuff.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Game{
+    private int count;
+    private String wordOfTheDay;
+    
+    public Game(int count, String wordOfTheDay){
+        this.count = count;
+        this.wordOfTheDay = wordOfTheDay;
+    }
+    
+    public int getCount(){
+        return count;
+    }
+    
+    public String getWordOfTheDay(){
+        return wordOfTheDay;
+    }
+    
+    public void askUser(){
+       Scanner guess = new Scanner(System.in);
+       System.out.println("Enter a Five Letter Word:");
+       String answer = guess.nextLine();
+       System.out.println("You guessed" + answer);
+    }
+    
+    public String analyzeUserInput(String answer){
+        int count = 0;
+        while(count<7){
+            //giant method start
+            
+            //method must be able to return inputted word in three different colors
+            //method must scan each letter in user input and compare it to wordOfTheDay
+            //method should print the letters in user input in green if the letter is guessed in the correct spot
+            //method should print the letters in user input in yellow if the letter is guessed correctly but not in the correct spot
+            //method should print the letters in user input in red if the letter is guessed wrong and not located in wordOfTheDay
+            //method count up after each time the user input is evaluated.
+            //if user input is not exactly 5 letters, count doesn't go up.  Instead method should print.  Try a word with 5 letters and repeat ask user method again.  Then analyze user input should be applied to that word.
+            //count goes up if user input is exactly five letters but after it is decided which color each letter will be.
+            
+            //giant method end
+        }
+    }
+}
+
+public class MyClass {
+    public static void main(String args[]) {
+      System.out.println("Hi there!  Guess the five letter word in 6 tries!");
+      System.out.println("Green means that the letter is in the word and in the correct position, Yellow means that the letter");
+      System.out.println("is in the word but the wrong position, Red means that the letter is not in the word.");
+    }
+}

041ea7dfa66c16386e41aae523c3220374bcabc0
VV1102 <105758475+VV1102@users.noreply.github.com>
2022-06-07 21:35:19 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,110 +1,57 @@
 import java.util.Scanner;
-import java.awt.*;
-import javax.swing.*;
+import java.util.Arrays;
+
 
 public class Game{
-    private int count;
-    private String wordOfTheDay;
-    
-    
+    private String wordOfTheDay = "games";
+    private int maxWordSize = 5;
+
     public Game(){
         //empty constructor
-    }
-    
-    public int getCount(){
-        return count;
     }
     
     public String getWordOfTheDay(){
         return wordOfTheDay;
     }
-    
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
-    public static final String YELLOW = "\033[0;33m";  // YELLOW
-    
-    
-    public String analyzeUserInput(String answer){
-        int count = 0;
-        String wordOfTheDay = "house";
-        
-        while(count<7){
-            
-            //method must be able to return inputted word in three different colors
-            //method must scan each letter in user input and compare it to wordOfTheDay
-            //method should print the letters in user input in green if the letter is guessed in the correct spot
-            //method should print the letters in user input in yellow if the letter is guessed correctly but not in the correct spot
-            //method should print the letters in user input in red if the letter is guessed wrong and not located in wordOfTheDay
-            //method count up after each time the user input is evaluated.
-            //if user input is not exactly 5 letters, count doesn't go up.  Instead method should print.  Try a word with 5 letters and repeat ask user method again.  Then analyze user input should be applied to that word.
-            //count goes up if user input is exactly five letters but after it is decided which color each letter will be.
-                        
-            
-            //giant method start
-            
-       Scanner guess = new Scanner(System.in);
-       System.out.println("Enter a Five Letter Word:");
-       String answer = guess.nextLine();
-       System.out.println("You guessed " + answer);
-            
-       String[] userGuess = new String[5];
-      
-                for(int i = 0; i < answer.length; i++){
-                   userGuess[i] = answer.substring(i);
-                }
-           
-            if(answer.length() == 5){
-            
-            if(answer.equals(wordOfTheDay)){
-                System.out.println(ANSI_GREEN + answer + ANSI_RESET);
-                count++;
-            }
-             else{
-                   for(int j = 0; j < answer.length; j++){
-                       if(userGuess[i].equals(wordOfTheDay.substring(i))){
-                            System.out.print(ANSI_GREEN + userGuess[i] + ANSI_RESET);
-                           count++;
-                       }
-                       else if(!userGuess[i].equals(wordOfTheDay.substring(i))){
-                            for(int k = j+1; k < answer.length; k++){
-                               if(userGuess[i].equals(wordOfTheDay.substring(k))){
-                                   System.out.print(ANSI_YELLOW + userGuess[i] + ANSI_RESET);
-                                   count++;
-                               }
-                               else{
-                                   System.out.print(ANSI_RED + userGuess[i] + ANSI_RESET);
-                                   count++;
-                               }
-                            }
-                       }
-                       else{
-                          System.out.println(ANSI_RED + answer + ANSI_RESET);
-                          count++; 
-                       }
-                
-                 
-                  }
-             }
-                
-                
-            
-            }
-            
-            else{
-                    System.out.println("Please enter a word that is 5 letters in length");
-                    Scanner guess = new Scanner(System.in);
-                    System.out.println("Enter a Five Letter Word:");
-                    String answer = guess.nextLine();
-                    System.out.println("You guessed " + answer);
-                    count = count;
+   
+   
+    public boolean wordContains(char p){
+       boolean contains=false;    
+       for (int v=0;v<wordOfTheDay.length();v++){
+           if(wordOfTheDay.charAt(v)==p)
+             contains = true;
        }
-            
-            //giant method end
-        }
-        
-        System.out.println("Whoops sorry!  You are out of tries.  Try again tomorrow.");
-        System.out.println("The correct word was: " + wordOfTheDay);
-            
-       }
-        
+        return contains;
     }
+   
+    public String analyzeUserInput(){
+       System.out.println("Enter a Five Letter Word:");
+       Scanner guess = new Scanner(System.in);
+       String userGuess = guess.nextLine();
+       System.out.println("You entered Five Lettered Word : "+ userGuess);
+      
+       String output = ""; 
+       
+       if (userGuess.length() > maxWordSize){
+            output = "Larger String" ;
+       }else{
+           for(int i=0;i <userGuess.length(); i++){
+              if (wordOfTheDay.charAt(i)== userGuess.charAt(i)){
+                //System.out.println(userGuess.charAt(i)+"* ");
+                output = output + userGuess.charAt(i)+"* ";
+              }else{
+                  if(wordContains(userGuess.charAt(i))){
+                      output = output + userGuess.charAt(i)+"# ";
+                  }else{
+                    //System.out.println(userGuess.charAt(i)+"! ");
+                    output = output + userGuess.charAt(i)+"! ";
+                  }//if
+              }//if
+           }//for
+       }//if
+       //System.out.println("output from Game:"+output);
+       return output;
+        
+    }// end of analyzeUserInput
+    
+}// end of Game
